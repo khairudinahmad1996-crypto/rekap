40b846bd6cfc82d940871a1f10a735bc885f709a
khairudinahmad1996-crypto <khairudinahmad1996@gmail.com>
2026-07-21 23:15:22 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,17 @@
-self.addEventListener('push', (event) => {
-  event.waitUntil(
-    self.registration.showNotification('Notification Title', {
-      body: 'Notification Body Text',
-      icon: 'custom-notification-icon.png',
-    });
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(async () => {
+      const cache = await caches.open(CACHE_NAME);
+
+      // match the request to our cache
+      const cachedResponse = await cache.match(event.request);
+
+      // check if we got a valid response
+      if (cachedResponse !== undefined) {
+          // Cache hit, return the resource
+          return cachedResponse;
+      } else {
+        // Otherwise, go to the network
+          return fetch(event.request)
+      };
+  });
 });
